e572c6407ad87b977a2015d1f3f514c4e4053dfc
Yasuo Ohno <yasuo.ohno@gmail.com>
2018-09-30 17:00:01 +0900

    fixed url,sum and sumtype

--- automatic/fossil/tools/chocolateyinstall.ps1
+++ automatic/fossil/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definitio
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://www.fossil-scm.org/xfer/uv/fossil-w32-2.7.zip'
-  checksum      = '3f708bac91cbf7b8824cbbe16b2ef692efd33cff'
-  checksumType  = 'sha1'
+  url           = ''
+  checksum      = ''
+  checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
