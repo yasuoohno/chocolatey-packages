37deeefa9c81181c8e57a8db44fcc04c449783c6
Yasuo Ohno <yasuo.ohno@gmail.com>
2018-09-30 16:55:41 +0900

    add fossil packge.

---
+++ automatic/fossil/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = 'https://www.fossil-scm.org/xfer/uv/fossil-w32-2.7.zip'
+  checksum      = '3f708bac91cbf7b8824cbbe16b2ef692efd33cff'
+  checksumType  = 'sha1'
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/fossil/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$juvlist_api = 'https://fossil-scm.org/index.html/juvlist'
+$download_url = 'https://www.fossil-scm.org/xfer/uv/'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $juv = Invoke-WebRequest -Uri $juvlist_api | ConvertFrom-Json
+  $latest = $juv | Where-Object {$_.name -like 'fossil-w32-*.zip'} | Sort-Object -Descending -Property mtime | Select-Object -First 1
+  if ( $latest -notmatch "fossil-w32-(?<version>.*?)\.zip" ) {
+  }
+
+  $version = $Matches.version
+  $url = $download_url + $latest.name
+  $OriginalHash = $latest.hash
+
+  @{
+    URL32 = $url
+    Version = $version
+    OriginalHash = $sum
+  }
+}
+
+update -ChecksumFor 32
