0c7fc126cf04c672d27cc112c5af1efffc00fdf9
yasuoohno <yasuo.ohno@gmail.com>
2018-11-11 00:22:46 +0900

    added puttyjp

---
+++ automatic/puttyjp/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName           = "$env:ChocolateyPackageName"
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url            = 'http://hp.vector.co.jp/authors/VA024651/download/file/putty-0.70-jp20170713.zip'
+  checksum       = '09324063971f12cecd5ce1f83114683101d7a440ab14aad7fa3ac06fb0c7fe70'
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/puttyjp/tools/chocolateyuninstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName           = "$env:ChocolateyPackageName"
