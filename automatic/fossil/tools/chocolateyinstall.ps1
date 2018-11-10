56623757a13682e4852943b1379f70cfbc59c0f7
yasuoohno <yasuo.ohno@gmail.com>
2018-11-10 22:31:17 +0900

    updated.

--- automatic/fossil/tools/chocolateyinstall.ps1
+++ automatic/fossil/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definitio
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = ''
-  checksum      = ''
+  url           = 'https://www.fossil-scm.org/xfer/uv/fossil-w32-2.7.zip'
+  checksum      = 'd245dd626f755bfce65265e4b5a5000df15ff3e1c0f4da0838b3bbd953eb32a0'
   checksumType  = 'sha256'
 }
 
--- automatic/kotlin/tools/chocolateyinstall.ps1
+++ automatic/kotlin/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $binPath               = "$toolsDir\kotlinc\bin"
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
-  url64bit       = ''
-  checksum64     = ''
+  url64bit       = 'https://github.com/JetBrains/kotlin/releases/download/v1.3.0/kotlin-compiler-1.3.0-release-windows-x64.zip'
+  checksum64     = '82d896cfa6c53eb2c2475c9683058e078af1fa07ec092e8f40bcccec194d0caa'
   checksumType64 = 'sha256'
 }
 
