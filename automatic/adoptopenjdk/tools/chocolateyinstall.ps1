acbf8a31f0e0957f13621842bd493014d3cd4019
yasuoohno <yasuo.ohno@gmail.com>
2019-02-02 04:02:20 +0900

    added adoptopenjdk

---
+++ automatic/adoptopenjdk/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿function Get-RedirectedUrl
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+
+    If ($response.StatusCode -eq "Found")
+    {
+        $response.GetResponseHeader("Location")
+    }
+}
+
+$java       = '11'
+$jvm        = 'hotspot'
+$arch       = 'x64'
+$release    = '11.0.2+9'
+$type       = 'jdk'
+$releaseURL = $type + '-' + [System.Web.HttpUtility]::UrlEncode($release)
+
+$apiurl= "https://api.adoptopenjdk.net/v2/binary/releases/openjdk$java`?os=windows&openjdk_impl=$jvm&arch=$arch&release=$releaseURL&type=$type"
+
+$packageName = $env:ChocolateyPackageName
+$targetDir   = 'C:\Program Files\AdoptOpenJDK'
+$url64       = Get-RedirectedUrl $apiurl
+$checksum64  = 'bde1648333abaf49c7175c9ee8ba9115a55fc160838ff5091f07d10c4bb50b3a'
+ 
+Install-ChocolateyZipPackage $packageName $url64 $targetDir
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\$type-$release 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath "$targetDir\$type-$release\bin" -PathType 'Machine'
---
+++ automatic/adoptopenjdk/tools/chocolateyuninstall.ps1
@@ -0,0 +1,37 @@
+﻿$targetDir    = 'C:\Program Files\AdoptOpenJDK'
+$release      = '11.0.2+9'
+$type         = 'jdk'
+
+$uninstallDir = "$targetDir\$type-$release"
+$removePath   = "$targetDir\$type-$release\bin"
+$pathType     = 'Machine'
+
+Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' 'Machine'
+Remove-Item -r $uninstallDir
+
+# remove from PATH.
+if ($env:PATH.ToLower().Contains($removePath.ToLower()))
+{
+  $statementTerminator = ";"
+  Write-Host "PATH environment variable contains $removePath. Removing..."
+  $actualPath = [System.Collections.ArrayList](Get-EnvironmentVariable -Name 'Path' -Scope $pathType).split($statementTerminator)
+ 
+  $actualPath.Remove($removePath)
+  $newPath =  $actualPath -Join $statementTerminator
+ 
+  if ($pathType -eq [System.EnvironmentVariableTarget]::Machine) {
+    if (Test-ProcessAdminRights) {
+      Set-EnvironmentVariable -Name 'Path' -Value $newPath -Scope $pathType
+    } else {
+      $psArgs = "UnInstall-ChocolateyPath -pathToUnInstall `'$originalPathToUnInstall`' -pathType `'$pathType`'"
+      Start-ChocolateyProcessAsAdmin "$psArgs"
+    }
+  } else {
+    Set-EnvironmentVariable -Name 'Path' -Value $newPath -Scope $pathType
+  }
+}
+
+# if there should be no other versions, remove targetDir.
+If ((Get-ChildItem -Force $targetDir) -eq $Null) {
+    Remove-Item $targetDir
+}
