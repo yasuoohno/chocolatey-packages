9e3fede27dedc79ce0daa599392f1904e393db98
Yasuo Ono <yasuo.ohno@gmail.com>
2020-07-20 11:29:45 +0900

    wip

--- automatic/winlibs-mingw-llvm/tools/chocolateyuninstall.ps1
+++ automatic/winlibs-mingw-llvm/tools/chocolateyuninstall.ps1
@@ -1,2 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName           = "$env:ChocolateyPackageName"
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$binPath               = "$toolsDir\mingw64\bin"
+
+#Using .NET method prevents expansion (and loss) of environment variables (whether the target of the removal or not)
+#To avoid bad situations - does not use substring matching or regular expressions
+#Removes duplicates of the target removal path, Cleans up double ";", Handles ending "\"
+
+$PathToRemove = $binPath
+foreach ($path in [Environment]::GetEnvironmentVariable("PATH","Machine").split(';'))
+{
+  If ($Path)
+  {
+    If (($path -ine "$PathToRemove") -AND ($path -ine "$PathToRemove\"))
+    {
+      [string[]]$Newpath += "$path"
+    }
+  }
+}
+$AssembledNewPath = ($newpath -join(';')).trimend(';')
+
+[Environment]::SetEnvironmentVariable("PATH",$AssembledNewPath,"Machine")
