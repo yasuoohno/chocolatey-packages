ecbc5d9aad4544efd68871dda5d40019248f8683
Yasuo Ono <yasuo.ohno@gmail.com>
2020-07-21 18:23:52 +0900

    remove url and hash

--- automatic/winlibs-mingw-llvm/tools/chocolateyinstall.ps1
+++ automatic/winlibs-mingw-llvm/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $binPath               = "$toolsDir\mingw64\bin"
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
-  url64bit       = 'https://github.com/brechtsanders/winlibs_mingw/releases/download/10.1.0-10.0.0-7.0.0-r3/winlibs-x86_64-posix-seh-gcc-10.1.0-llvm-10.0.0-mingw-w64-7.0.0-r3.7z'
-  checksum64     = '376354566491b56b87ff2bc9a28971902967229a51d541293c75f53db3f490fb'
+  url64bit       = ''
+  checksum64     = ''
   checksumType64 = 'sha256'
 }
 
